309c1f052e521e0998eb5eb0598ce9127e24853c
Braxton Ellsworth <business@pcsmilesai.com>
2025-08-01 16:59:32 -0400

    ✅ Fix: wrap App in BrowserRouter to enable routing

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,10 @@ import './index.css';
 // React.StrictMode to catch potential problems during
 // development. The HTML page is assumed to have a root
 // element with id="root" where the app will be rendered.
+import { BrowserRouter } from 'react-router-dom';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter><App /></BrowserRouter>
   </React.StrictMode>
 );
