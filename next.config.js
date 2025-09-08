1f98a5ee7565c6a80cc13da0bd0e481616987512
Braxton Ellsworth <business@pcsmilesai.com>
2025-09-08 09:53:06 -0400

    chore: use App Router wrappers; disable legacy pages dir

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    CUSTOM_KEY: process.env.CUSTOM_KEY,
-  },
-}
+  experimental: { appDir: true },
+  // Prevent Next from treating src/pages as the legacy Pages Router
+  // We only use the App Router wrappers under app/.
+  pageExtensions: ['not-used'], // effectively disables pages/*
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
