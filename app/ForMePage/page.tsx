c5dbc540b4acab3d338ff63186862351fb13352b
Braxton Ellsworth <business@pcsmilesai.com>
2025-09-09 09:45:07 -0400

    Force dynamic rendering for ForMePage and fix window.location issue

--- app/ForMePage/page.tsx
+++ app/ForMePage/page.tsx
@@ -1,6 +1,8 @@
 'use client';
 import ForMePageImpl from '../../src/ui-pages/ForMePage.jsx';
 
+export const dynamic = 'force-dynamic';
+
 export default function Page(props: any) {
   console.log('🔍 ForMePage Page component props:', props);
   return <ForMePageImpl {...props} />;
